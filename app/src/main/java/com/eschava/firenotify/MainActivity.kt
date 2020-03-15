ac4db0da68c9715af91025caa98f7a6ea511b058
Eugene Schava <eschava@gmail.com>
2020-03-15 09:40:35 +0200

    migrate to AndroidX

--- app/src/main/java/com/eschava/firenotify/MainActivity.kt
+++ app/src/main/java/com/eschava/firenotify/MainActivity.kt
@@ -5,8 +5,8 @@ import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.iid.FirebaseInstanceId
 import kotlinx.android.synthetic.main.activity_main.*
 
